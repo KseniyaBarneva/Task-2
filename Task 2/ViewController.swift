61bd16051033e95d5e170e971af46a8e449bc505
Kseniya Barneva <kseniya.barneva@mail.ru>
2021-06-18 19:34:19 +0300

    Initial Commit

---
+++ Task 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 2
+//
+//  Created by Ксения on 18.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
